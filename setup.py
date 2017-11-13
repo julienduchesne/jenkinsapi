71390498c7c76831de5c36f3ed63f895cfd8ef87
Julien Duchesne <jduchesne@coveo.com>
2017-11-13 11:52:54 -0500

    Deploy to pypi internal cloud

--- setup.py
+++ setup.py
@@ -1,6 +1,23 @@
-from setuptools import setup
+#!/usr/bin/env python
+# coding=utf-8
 
+import json
+import os
 
+from setuptools import setup, find_packages
+
+package_root = os.path.dirname(os.path.realpath(__file__))
 setup(
-    setup_requires=['pbr'],
-    pbr=True)
+    name="jenkinsapi",
+    version="0.4.1",
+    description="JenkinsAPI fork",
+    maintainer="Julien Duchesne",
+    maintainer_email="jduchesne@coveo.com",
+    url="",
+    install_requires=[
+        "pytz>=2014.4",
+        "requests>=2.3.0",
+        "six>=1.10.0"
+    ],
+    packages=find_packages()
+)
